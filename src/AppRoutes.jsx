53e8d887cb713b01eed9a95a588985fb9dc24fe9
visvesvaran62 <visvesvaran62@gmail.com>
2026-03-26 23:43:10 +0530

    update

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Assignments from './pages/Assignments'
-import Assign21 from './ReactAssignment/Assign-21/Assgn21'
 
+import Assign21 from './ReactAssignment/Assign-21/Assgn21'
 import Assign26 from './ReactAssignment/day-26/Assign26'
 import Receipe from './ReactAssignment/reciepes/Receipe'
 import Assign25 from './ReactAssignment/day25/Assign25'
 
-
 const AppRoutes = () => {
   return (
-    <>
     <Routes>
-        <Route path='/' element={<Home/>}/>
-       <Route path='/assignment' element={<Assignments/>}/>
-       <Route path='/assignments/1' element={<Assign21/>}/>
-      <Route path="/assignment/22" element={<Receipe/>}/>
-       <Route path='/assignments/26' element={<Assign26/>}/>
-       <Route path="/assignments/25" element={<Assign25/>}/>
-      
+      <Route path='/' element={<Home />} />
+
+      {/* ✅ FIXED */}
+      <Route path='/assignments' element={<Assignments />} />
+
+      {/* ✅ MATCHING IDs */}
+      <Route path='/assignments/21' element={<Assign21 />} />
+      <Route path='/assignments/22' element={<Receipe />} />
+      <Route path='/assignments/25' element={<Assign25 />} />
+      <Route path='/assignments/26' element={<Assign26 />} />
 
     </Routes>
-    </>
   )
 }
 
