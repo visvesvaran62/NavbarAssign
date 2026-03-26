342257ae0c1ff0214eeff0c9007fd67967140ee5
visvesvaran62 <visvesvaran62@gmail.com>
2026-03-26 18:17:41 +0530

    msg

--- src/ReactAssignment/reciepes/components/Products.jsx
+++ src/ReactAssignment/reciepes/components/Products.jsx
@@ -1,47 +1,34 @@
-import React, { useEffect, useState  } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 
 
 const Products = () => {
- const[datas,setDatas]=useState([])
- const Navigate=useNavigate()
+  const [datas, setDatas] = useState([])
+  
 
-    const fetchedData=async()=>{
-        try{
-            const data= await fetch("https://dummyjson.com/recipes")
-            const res= await data.json()
-             setDatas(res.recipes)
-    
-
-           
-        }catch(e){
-            console.log("error",e);
-            
-
-        }
+  useEffect(() => {
+    const fetchedData = async () => {
+      try {
+        const data = await fetch("https://dummyjson.com/recipes")
+        const res = await data.json()
+        setDatas(res.recipes)
+      } catch (e) {
+        console.log("error", e)
+      }
     }
-    
-      useEffect(()=>{
-                fetchedData()
-            }, [ ])
 
-  
+    fetchedData()
+  }, [])
 
-   
   return (
-    <>
-    <div className='bg-black p-10 flex flex-wrap gap-8 justify-center '>
-       {datas.map((e)=>(
-        <div key={e.id} className='bg-amber-300 text-black w-100 h-100 rounded-2xl border-2 flex  justify-center items-center flex-col'>
-            <img src={e.image} className="  p-30rounded-2xl flex items-center h-70 w-70 justify-center" />
-            <h1>{e.name}</h1>
-            <p className='font-bold'>Ratings:{e.rating}</p>
-             </div>
-       ))}
+    <div className='bg-black p-10 flex flex-wrap gap-8 justify-center'>
+      {datas.map((e) => (
+        <div key={e.id} className='bg-amber-300 text-black w-[300px] h-[300px] rounded-2xl border-2 flex justify-center items-center flex-col'>
+          <img src={e.image} alt={e.name} className="p-3 rounded-2xl h-40 w-40" />
+          <h1>{e.name}</h1>
+          <p className='font-bold'>Ratings: {e.rating}</p>
+        </div>
+      ))}
     </div>
-  
- 
-    </>
   )
 }
 
