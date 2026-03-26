65fd506e2fddb7d8fa9f0385f24e3c9c592a6656
visvesvaran62 <visvesvaran62@gmail.com>
2026-03-26 23:59:00 +0530

    changes

--- src/ReactAssignment/reciepes/components/Products.jsx
+++ src/ReactAssignment/reciepes/components/Products.jsx
@@ -21,14 +21,21 @@ const Products = () => {
 
   return (
     <div className='bg-black p-10 flex flex-wrap gap-8 justify-center'>
-      {datas.map((e) => (
-        <div key={e.id} className='bg-amber-300 text-black w-30 h-10 rounded-2xl border-2 flex justify-center items-center flex-col'>
-          <img src={e.image} alt={e.name} className="p-3 rounded-2xl h-40 w-40" />
-          <h1>{e.name}</h1>
-          <p className='font-bold'>Ratings: {e.rating}</p>
-        </div>
-      ))}
+  {datas.map((e) => (
+    <div
+      key={e.id}
+      className='bg-amber-300 text-black w-64 rounded-2xl border-2 p-4 flex flex-col items-center shadow-lg hover:scale-105 transition'
+    >
+      <img
+        src={e.image}
+        alt={e.name}
+        className="rounded-xl h-40 w-full object-cover mb-3"
+      />
+      <h1 className="font-semibold text-center">{e.name}</h1>
+      <p className='font-bold'>⭐ {e.rating}</p>
     </div>
+  ))}
+</div>
   )
 }
 
