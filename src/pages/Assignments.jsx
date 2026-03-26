1782413734982b6f41884d346aa39df5015bdb62
visvesvaran62 <visvesvaran62@gmail.com>
2026-03-26 23:12:47 +0530

    changes

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -1,7 +1,5 @@
-import { Link } from "react-router-dom"
-
-
-import { assignmentData } from "./data"; 
+import AssignmentCard from "./AssignmentCard";
+import { assignmentData } from "./data";
 
 const Assignments = () => {
   return (
@@ -14,25 +12,7 @@ const Assignments = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
 
         {assignmentData.map((item) => (
-          <div
-            key={item.id}
-            className="backdrop-blur-lg bg-white/20 border border-white/30 rounded-2xl p-6 shadow-lg hover:scale-105 hover:shadow-2xl transition duration-300"
-          >
-            <h2 className="text-2xl font-semibold text-white mb-2">
-              {item.title}
-            </h2>
-
-            <p className="text-white/80 mb-4">
-              {item.desc}
-            </p>
-
-            <Link
-              to={`/assignments/${item.id}`}
-              className="inline-block bg-white text-purple-600 font-medium px-4 py-2 rounded-lg hover:bg-purple-600 hover:text-white transition"
-            >
-              View →
-            </Link>
-          </div>
+          <AssignmentCard key={item.id} item={item} />
         ))}
 
       </div>
